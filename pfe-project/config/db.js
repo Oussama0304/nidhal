083e5f7ba9286d54745ab44871c5f4b551869de1
Oussama0304 <oussama.boughanmi@esprit.tn>
2024-12-29 15:46:10 +0100

    oommmm'

--- pfe-project/config/db.js
+++ pfe-project/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-    host: 'mysql',
-    user: 'root',
-    password: '',
-    database: 'ProjetPfeAgil',
+    host: process.env.DB_HOST || 'mysql',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'ProjetPfeAgil',
     connectionLimit: 10
 });
 
